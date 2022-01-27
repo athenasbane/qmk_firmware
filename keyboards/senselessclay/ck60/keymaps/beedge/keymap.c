e9950c965284cad672493d281258c22d994a936e
Benjamin Collingwood <benjamincollingwood@me.com>
2022-01-27 14:49:17 +0000

    Change backtick right of leftshift and have green consistant with red direction keys

--- keyboards/senselessclay/ck60/keymaps/beedge/keymap.c
+++ keyboards/senselessclay/ck60/keymaps/beedge/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
-        KC_LSFT, KC_GRAVE, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,            KC_RSFT,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,            KC_RSFT,
         KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_LEFT, KC_DOWN, KC_RGHT, MO(1)
     ),
     [_1] = LAYOUT_60_iso(
@@ -56,6 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void rgb_matrix_indicators_user(void)
 {
+    rgb_matrix_set_color_all(RGB_GREEN);
  	if (host_keyboard_led_state().caps_lock)
 	{
 		rgb_matrix_set_color(28, RGB_RED);
@@ -69,6 +70,6 @@ void rgb_matrix_indicators_user(void)
 	}
 	if (IS_LAYER_ON(_1))
 	{
-		rgb_matrix_set_color(22, 200, 0, 200);
+		rgb_matrix_set_color(42, RGB_RED);
 	}
 }
